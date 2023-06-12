910fd29b01af7ac4103310938a9d6d9f2c16de2c
jessimiller <jcmillers13@gmail.com>
2023-06-12 16:39:28 -0400

    added static elements to account screen

--- app/src/main/java/com/example/mediasoundfilter/screens/account/AccountScreen.kt
+++ app/src/main/java/com/example/mediasoundfilter/screens/account/AccountScreen.kt
@@ -1,8 +1,28 @@
 package com.example.mediasoundfilter.screens.account
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.mediasoundfilter.R
 
 @Composable
 fun AccountScreen() {
-
+    Column() {
+        Text(
+            text = "insert dynamic content here"
+        )
+        Text(
+            text = stringResource(R.string.saved)
+        )
+        //dynamic block
+        Row() {
+//            Image(
+//                painter = painterResource(null),
+//                contentDescription = null
+//            )
+            Text("insert more dynamic content here")
+        }
+    }
 }
