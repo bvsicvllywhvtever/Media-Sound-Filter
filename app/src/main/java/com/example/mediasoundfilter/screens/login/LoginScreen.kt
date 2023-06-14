0a4a007924fd3808c92169c10c7011088412dff2
jessimiller <jcmillers13@gmail.com>
2023-06-14 16:00:14 -0400

    added detailed styling to login screen

--- app/src/main/java/com/example/mediasoundfilter/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/mediasoundfilter/screens/login/LoginScreen.kt
@@ -1,20 +1,29 @@
 package com.example.mediasoundfilter.screens.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,10 +31,14 @@ import com.example.mediasoundfilter.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+//fun LoginScreen() {
 fun LoginScreen(navigateToUpload: () -> Unit, navigateToCreateAccount: () -> Unit) {
     Column(
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
     ) {
         Text(
             text = stringResource(R.string.title),
@@ -35,34 +48,57 @@ fun LoginScreen(navigateToUpload: () -> Unit, navigateToCreateAccount: () -> Uni
         Column(
             modifier = Modifier.padding(15.dp)
         ) {
-            TextField(
+            OutlinedTextField(
                 value = "",
                 onValueChange = {},
+                label = {Text(stringResource(R.string.username))},
+                leadingIcon = {
+                    Image(
+                        painter = painterResource(R.drawable.account),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(colorResource(R.color.main))
+                    )},
                 shape = RoundedCornerShape(10.dp),
                 modifier = Modifier.padding(15.dp)
             )
-            TextField(
+            OutlinedTextField(
                 value = "",
                 onValueChange = {},
+                label = {Text(stringResource(R.string.password))},
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = null,
+                        tint = colorResource(R.color.main)
+                    )},
+                visualTransformation = PasswordVisualTransformation(),
                 shape = RoundedCornerShape(10.dp),
                 modifier = Modifier.padding(15.dp)
             )
         }
         Button(
             onClick = navigateToUpload,
+            //{},
             shape = RoundedCornerShape(10.dp)
         ){
             Text(stringResource(R.string.login))
         }
-        Row {
+        Row (modifier = Modifier.padding(15.dp)) {
             Text(
                 text = stringResource(R.string.sign_up_message),
-                modifier = Modifier.padding(15.dp)
+                modifier = Modifier.padding(0.dp, 0.dp, 3.dp, 0.dp)
             )
             Text(
                 text = stringResource(R.string.sign_up_link),
+                color = colorResource(R.color.link),
                 modifier = Modifier.clickable(onClick = navigateToCreateAccount)
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    //LoginScreen()
 }
