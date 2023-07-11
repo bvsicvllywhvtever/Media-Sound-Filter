3094d6292c2b6f38cc22406bcb6a27d5f70ee30c
jessimiller <jcmillers13@gmail.com>
2023-07-11 10:51:50 -0400

    added some comments and updated gitignore

--- app/src/main/java/com/example/mediasoundfilter/viewmodel/UploadViewModel.kt
+++ app/src/main/java/com/example/mediasoundfilter/viewmodel/UploadViewModel.kt
@@ -13,36 +13,44 @@ class UploadViewModel: ViewModel() {
 
     fun extractVideoId(link: String){
 
+        //check if link is empty
         if(link == ""){
             _uploadUiState.value = _uploadUiState.value.copy(linkError = "Link field cannot be left empty.")
         }
+        //otherwise extract videoId
         else{
+            //possible valid regex formats
             val pattern1 = Regex("""^https://www\.youtube\.com/watch\?v=([A-Za-z0-9])+${'$'}""")
             val pattern2 = Regex("""^www\.youtube\.com/watch\?v=([A-Za-z0-9])+${'$'}""")
             val pattern3 = Regex("""^youtube\.com/watch\?v=([A-Za-z0-9])+${'$'}""")
             val pattern4 = Regex("""^https://youtu\.be/([A-Za-z0-9])+${'$'}""")
             val pattern5 = Regex("""^youtu\.be/([A-Za-z0-9])+${'$'}""")
 
+            //check if link matches any of the possible formats and get the videoId
             var id: String? = null
-
+            //patterns 1-3 are typical youtube urls
             if(pattern1.containsMatchIn(link) || pattern2.containsMatchIn(link) || pattern3.containsMatchIn(link)){
                 id = link.substringAfter('=')
             }
+            //patterns 4-5 are typical share youtube urls
             else if(pattern4.containsMatchIn(link) || pattern5.containsMatchIn(link)){
                 id = link.substringAfterLast('/')
             }
 
-            //TODO: check if videoId is valid/null
-            val linkIsValid = true
+            //check if videoId is valid, and update state accordingly
+            val idIsValid = isVideoIdValid(id)
 
-            if(linkIsValid){
+            if(idIsValid){
                 _uploadUiState.value = _uploadUiState.value.copy(videoId = id)
             }
             else{
                 _uploadUiState.value = _uploadUiState.value.copy(linkError = "Youtube link is not valid.")
             }
-
         }
+    }
+
+    private fun isVideoIdValid(id: String?): Boolean{
+        return false
     }
 
     fun resetState(){
