553843aefb760f46fb3a163c2ddff7fd495a5078
jessimiller <jcmillers13@gmail.com>
2023-07-27 08:55:25 -0400

    added navbar to sounds screen

--- app/src/main/java/com/example/mediasoundfilter/ui/screens/upload/SoundsScreen.kt
+++ app/src/main/java/com/example/mediasoundfilter/ui/screens/upload/SoundsScreen.kt
@@ -1,10 +1,14 @@
 package com.example.mediasoundfilter.ui.screens.upload
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -13,7 +17,9 @@ import androidx.navigation.compose.rememberNavController
 import com.example.mediasoundfilter.R
 import com.example.mediasoundfilter.ui.components.AdaptableGrid
 import com.example.mediasoundfilter.ui.components.ExpandableCard
+import com.example.mediasoundfilter.ui.components.NavBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SoundsScreen(uploadViewModel: UploadViewModel, navController: NavController) {
 
@@ -22,24 +28,35 @@ fun SoundsScreen(uploadViewModel: UploadViewModel, navController: NavController)
 
     uploadViewModel.setSounds()
 
-    Column(){
-        Text(stringResource(R.string.mute_sounds_title) + " " + "placeholder" + "?")
+    Scaffold(
+        content = { padding ->
+            Column(modifier = Modifier.padding(padding)) {
+                Text(stringResource(R.string.mute_sounds_title) + " " + "placeholder" + "?")
 
-        val soundCategories = uploadUiState.value.muteSounds.keys
-        for (cat in soundCategories){
-            ExpandableCard(cat){
-                uploadUiState.value.muteSounds[cat]?.let { AdaptableGrid(2, it, uploadViewModel) }
-            }
-        }
+                val soundCategories = uploadUiState.value.muteSounds.keys
+                for (cat in soundCategories) {
+                    ExpandableCard(cat) {
+                        uploadUiState.value.muteSounds[cat]?.let {
+                            AdaptableGrid(
+                                2,
+                                it,
+                                uploadViewModel
+                            )
+                        }
+                    }
+                }
 
-        Button(
-            content = {Text(stringResource(R.string.watch_video))},
-            onClick = {
-                navController.navigate("media/$videoId")
-                uploadViewModel.resetState()
+                Button(
+                    content = { Text(stringResource(R.string.watch_video)) },
+                    onClick = {
+                        navController.navigate("media/$videoId")
+                        uploadViewModel.resetState()
+                    }
+                )
             }
-        )
-    }
+        },
+        bottomBar = {NavBar(navController)}
+    )
 }
 
 @Preview(showBackground = true)
